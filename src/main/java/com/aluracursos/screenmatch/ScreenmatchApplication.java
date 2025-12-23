20deae52a8d20b3d9e59527ccb8a66ddb5a64c10
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2025-12-22 23:22:19 -0300

    Prueba Mapeo de Serie GetMapping

--- src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -7,32 +7,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Clase principal de Spring Boot que inicia la aplicación y ejecuta
- * el menú CLI (clase Principal) usando el repositorio inyectado.
- *
- * Nota: la ejecución del menú se realiza en el hilo principal y espera
- * entradas por consola; esto es adecuado para aplicaciones de ejemplo/CLI.
- */
 @SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-    @Autowired
-    private SerieRepository repository;
+public class ScreenmatchApplication  {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// Se crea la clase "Principal" que contiene la lógica del menú CLI.
-		// Usamos el repositorio inyectado para que la capa de presentación
-		// pueda persistir las entidades consultadas.
-		Principal principal = new Principal(repository);
-		principal.muestraElMenu();
-
-
-
 
 	}
 }
---
+++ src/main/java/com/aluracursos/screenmatch/ScreenmatchApplicationConsola.java
@@ -0,0 +1,40 @@
+//package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.principal.Principal;
+import com.aluracursos.screenmatch.repositorio.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Clase principal de Spring Boot que inicia la aplicación y ejecuta
+ * el menú CLI (clase Principal) usando el repositorio inyectado.
+ *
+ * Nota: la ejecución del menú se realiza en el hilo principal y espera
+ * entradas por consola; esto es adecuado para aplicaciones de ejemplo/CLI.
+ */
+/*
+@SpringBootApplication
+public class ScreenmatchApplicationConsola implements CommandLineRunner {
+    @Autowired
+    private SerieRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplicationConsola.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Se crea la clase "Principal" que contiene la lógica del menú CLI.
+		// Usamos el repositorio inyectado para que la capa de presentación
+		// pueda persistir las entidades consultadas.
+		Principal principal = new Principal(repository);
+		principal.muestraElMenu();
+
+
+
+
+	}
+}
+*/
