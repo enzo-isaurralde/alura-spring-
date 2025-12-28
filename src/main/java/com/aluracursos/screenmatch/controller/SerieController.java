cbcb0a5f71b7f78065854a69ffb2751cacf89d7c
Enzo Ariel Isaurralde <enzoarielisaurralde@gmail.com>
2025-12-27 23:13:32 -0300

    Cors para poder mostrar en front, en xml se agrego un dependencia Spring boot devtools

--- src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
+++ src/main/java/com/aluracursos/screenmatch/controller/SerieController.java
@@ -1,13 +1,27 @@
 package com.aluracursos.screenmatch.controller;
 
+import com.aluracursos.screenmatch.dto.SerieDTO;
+import com.aluracursos.screenmatch.repositorio.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 public class SerieController {
+
+    @Autowired
+    private SerieRepository repository;
+
     @GetMapping("/serie")
-    public String mostrarMensaje() {
-        return "¡Hola desde SerieController!";
+    public List<SerieDTO> obtenerTodasLasSeries() {
+        return repository.findAll().stream()
+                .map(s -> new SerieDTO(s.getTitulo(), s.getTotalTemporadas(), s.getEvaluacion(),
+                        s.getPoster(), s.getGenero(), s.getActores(), s.getSinopsis()))
+                .collect(Collectors.toList());
     }
+
 }
 
